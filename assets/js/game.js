149d9e393c051ee70c70d2b362625e5824d8f00e
juliana1390 <juliana_santos@usp.br>
2022-04-22 17:55:16 -0300

    finished functions
    - randomSort: randomly sorting cards;
    
    - flipCard: flipping cards, matches, unmatches;
    
    - matchCardSign: match check
    
    - victory: call victory page

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,6 +4,10 @@
 
     var memorizeModal = document.querySelector("#memorizeModal");
 
+    var matches = 0;
+
+    var imgMatchSign = document.querySelector("#imgMatchSign");
+
     // pushing images to array "images"
     for(var i = 0; i < 16; i++){
         // object img
@@ -19,10 +23,20 @@
     /* -------------------------------- STARTING the game -------------------------------- */
     function startGame(){
 
+        images = randomSort(images);
+
+        flippedCards = [];
+
+        matches = 0;
+
         var frontFaces = document.getElementsByClassName("front");
+        var backFaces = document.getElementsByClassName("back");
                 
         // distributing the cards throughout the container
         for(var i = 0; i < 16; i++){
+
+            frontFaces[i].classList.remove("flipped", "match");
+            backFaces[i].classList.remove("flipped", "match");
 
             var card = document.querySelector("#card" + i);
             
@@ -46,19 +60,106 @@
             frontFaces[i].setAttribute("id", images[i].id);
 
         }
-
+        
+        // hiding memorizeModal 
         memorizeModal.style.zIndex = -2;
         memorizeModal.removeEventListener("click", startGame,false);
     }
+    /* ------------------------------ randomly SORTING cards ------------------------------ */
+    // /**
+    //  * @param {*} images array with organized images
+    //  * @returns sorted - array with imagens in random positions
+    //  */
+    function randomSort(images){
+        
+        var sorted = [];
+
+        while(sorted.length !== images.length){
+            var i = Math.floor(Math.random() * images.length);
+
+            if(sorted.indexOf(images[i]) < 0){
+                sorted.push(images[i]);
+            }
+        }
+
+        return sorted;
+    }
     /* ---------------------------------- FLIPPING cards ---------------------------------- */
     function flipCard(){
-        var faces = this.getElementsByClassName("face");
+        
+        // when less the 2 cards was clicked
+        if(flippedCards.length < 2){
 
-        faces[0].classList.toggle("flipped");
-        faces[1].classList.toggle("flipped");
+            var faces = this.getElementsByClassName("face");
+
+            // it avoids functional double click in the same card
+            if(faces[0].classList.length > 2){
+                return;
+            }
+
+            // switching faces class to flipped  
+            faces[0].classList.toggle("flipped");
+            faces[1].classList.toggle("flipped");
+
+            // taking clicked cards to flippedCards array
+            flippedCards.push(this);
+
+            // when two cards were already clicked
+            if(flippedCards.length === 2){
+
+                // a match was made
+                if(flippedCards[0].childNodes[3].id === flippedCards[1].childNodes[3].id){
+
+                    //switching faces class to match
+                    flippedCards[0].childNodes[1].classList.toggle("match");
+                    flippedCards[0].childNodes[3].classList.toggle("match");
+                    flippedCards[1].childNodes[1].classList.toggle("match");
+                    flippedCards[1].childNodes[3].classList.toggle("match");
+
+                    // symbol shown when a match is made
+                    matchCardSign();
+
+                    // adding the class "pair" to "match"
+                    flippedCards[0].childNodes[1].classList.add("pair");
+                    flippedCards[0].childNodes[3].classList.add("pair");
+                    flippedCards[1].childNodes[1].classList.add("pair");
+                    flippedCards[1].childNodes[3].classList.add("pair");
+
+                    matches++;
+
+                    // array is now empty again
+                    flippedCards = [];
+
+                    // it call's the victory's page when 8 matches are made
+                    if(matches === 8){
+                        victory();
+                    }
+                }
+            }
+            
+        }else{
+            // in case of no match
+            flippedCards[0].childNodes[1].classList.toggle("flipped");
+            flippedCards[0].childNodes[3].classList.toggle("flipped");
+            flippedCards[1].childNodes[1].classList.toggle("flipped");
+            flippedCards[1].childNodes[3].classList.toggle("flipped");
+
+            // emptying array
+            flippedCards = [];
+        }        
     }
-
-    /* ---------------------------------- warning Modal ---------------------------------- */
+    /* ------------------------------------ MATCH check ------------------------------------ */
+    function matchCardSign(){
+        imgMatchSign.style.zIndex = 1;
+        imgMatchSign.style.top = 150 + "px";
+        imgMatchSign.style.opacity = 0;
+        setTimeout(function(){
+            imgMatchSign.style.zIndex = -1;
+            imgMatchSign.style.top = 250 + "px";
+            imgMatchSign.style.opacity = 1;
+            },1000);
+    }
+    /* ----------------------------------- warning Modal ----------------------------------- */
     window.setTimeout(function(){
         memorize();
     },200)
@@ -67,6 +168,11 @@
         memorizeModal.style.zIndex = 10;
         memorizeModal.addEventListener("click", startGame,false);
     }
+    /* --------------------------------- call VICTORY page --------------------------------- */
+    function victory(){
+        window.location.replace("victory.html");
+    }
+
 
 
 }());
