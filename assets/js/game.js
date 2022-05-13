ca17f23c6f95123ca8dc5028630b51b1e14f6073
juliana1390 <juliana_santos@usp.br>
2022-05-13 20:29:49 -0300

    code improvement
    game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -44,17 +44,8 @@
 
             var card = document.querySelector("#card" + i);
             
-            card.style.left = i % 4 === 0? 10 + "px" : i % 4 * 150 + 10 + "px"; 
-
-            if(i < 4){
-                card.style.top = 0 + "px";
-            } else if(i < 8){
-                card.style.top = 150 + "px";
-            }else if(i < 12){
-                card.style.top = 300 + "px";
-            } else{
-                card.style.top = 450 + "px";
-            }
+            card.style.left = (i % 4) * 150 + "px"; 
+            card.style.top = (Math.floor(i / 4) * 150)  + "px";
 
             // adding click event 
             card.addEventListener("click", flipCard, false);
@@ -130,19 +121,21 @@
                 if(flippedCards[0].childNodes[3].id === flippedCards[1].childNodes[3].id){
 
                     //switching faces class to match
-                    flippedCards[0].childNodes[1].classList.toggle("match");
-                    flippedCards[0].childNodes[3].classList.toggle("match");
-                    flippedCards[1].childNodes[1].classList.toggle("match");
-                    flippedCards[1].childNodes[3].classList.toggle("match");
+                    for(let i = 0; i < 2; i++){
+                        for(let j = 1; j < 4; j+=2){
+                            flippedCards[i].childNodes[j].classList.toggle("match");
+                        }
+                    }
 
                     // symbol shown when a match is made
                     matchCardSign();
 
                     // adding the class "pair" to "match"
-                    flippedCards[0].childNodes[1].classList.add("pair");
-                    flippedCards[0].childNodes[3].classList.add("pair");
-                    flippedCards[1].childNodes[1].classList.add("pair");
-                    flippedCards[1].childNodes[3].classList.add("pair");
+                    for(let i = 0; i < 2; i++){
+                        for(let j = 1; j < 4; j+=2){
+                            flippedCards[i].childNodes[j].classList.add("pair");
+                        }
+                    }
 
                     removeHover()
 
@@ -162,10 +155,11 @@
             
         }else{
             // in case of no match
-            flippedCards[0].childNodes[1].classList.toggle("flipped");
-            flippedCards[0].childNodes[3].classList.toggle("flipped");
-            flippedCards[1].childNodes[1].classList.toggle("flipped");
-            flippedCards[1].childNodes[3].classList.toggle("flipped");
+            for(let i = 0; i < 2; i++){
+                for(let j = 1; j < 4; j+=2){
+                    flippedCards[i].childNodes[j].classList.toggle("flipped");
+                }
+            }
 
             // emptying array
             flippedCards = [];
